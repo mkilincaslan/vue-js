90b7bab1e74066ac53d2c9de6fcd57c8a9622863
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-10-14 11:50:49 +0300

    animations app has been added

---
+++ animations/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h3>Animations & Transitions</h3>
+                <hr>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style>
+</style>
