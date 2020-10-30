032af7c0892cda5d06ea60f6fe513f0499a5d1c2
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-10-30 23:03:03 +0300

    getters & mapGetters

--- vuex/src/components/Result.vue
+++ vuex/src/components/Result.vue
@@ -1,16 +1,46 @@
 <template>
     <div>
         <p class="counter-container"> Sayaç : {{ counter }}</p>
+        <p class="counter-container"> 2 * Sayaç: {{ doubleCounter }}</p>
     </div>
 </template>
 <script>
+    import { mapGetters } from "vuex";
     export default {
-        // props : ["counter"],
+        // props : ["counter"], // Without vuex
+
+        // First way to get store variables - beginner
+        // computed: {
+        //     counter() {
+        //         return this.$store.getters.getCounter;
+        //     },
+        //     doubleCounter() {
+        //         return this.$store.getters.getDoubleCounter;
+        //     }
+        // }
+
+        // Second way to get store variables - intermediate -- mapGetters ile tüm store yapımızı daha kolay alabiliyoruz
+        // computed: mapGetters([
+        //     "getDoubleCounter",
+        //     "getCounter"
+        // ])
+
+        // Second way with keys - Anahtar kullanarak mapGetters faydalanma
+        // computed: mapGetters({
+        //     counter: 'getCounter',
+        //     doubleCounter: 'getDoubleCounter',
+        // })
+
+        // Third way to get store variables with custom computed functions -- Kendi computed fonksiyonlarımız ile kullanma yöntemi
         computed: {
-            counter() {
-                return this.$store.state.counter;
+            ...mapGetters({
+                counter: 'getCounter',
+                doubleCounter: 'getDoubleCounter',
+            }),
+            customText() {
+                return "";
             }
-        }
+        },
     }
 
 </script>
--- vuex/src/store/index.js
+++ vuex/src/store/index.js
@@ -6,7 +6,15 @@ Vue.use(Vuex);
 const store =  new Vuex.Store({
     state: {
         counter: 0   
-    }
+    },
+    getters: {
+        getDoubleCounter(state) {
+            return state.counter * 2;
+        },
+        getCounter(state) {
+            return state.counter;
+        }
+    },
 });
 
 export default store;
