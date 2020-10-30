af8733db4dbb9b0390ddb64ab0845a5ff9e48595
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-10-30 22:27:17 +0300

    update tab

--- vuex/src/components/Result.vue
+++ vuex/src/components/Result.vue
@@ -1,25 +1,25 @@
 <template>
-  <div>
-    <p class="counter-container"> Sayaç : {{ counter }}</p>
-  </div>
+    <div>
+        <p class="counter-container"> Sayaç : {{ counter }}</p>
+    </div>
 </template>
 <script>
-  export default {
-    // props : ["counter"],
-    computed: {
-      counter() {
-        return this.$store.state.counter;
-      }
+    export default {
+        // props : ["counter"],
+        computed: {
+            counter() {
+                return this.$store.state.counter;
+            }
+        }
     }
-  }
 
 </script>
 <style scoped>
-  .counter-container{
-    border: 1px solid #aa7f08;
-    padding: 10px 5px;
-    background-color: #fbbd08;
-    font-size: 20px;
-    color: #000;
-  }
+    .counter-container{
+        border: 1px solid #aa7f08;
+        padding: 10px 5px;
+        background-color: #fbbd08;
+        font-size: 20px;
+        color: #000;
+    }
 </style>
