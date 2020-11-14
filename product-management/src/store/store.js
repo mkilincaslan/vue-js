3f7cb174cfd3100ee92cb465da3abc77e12eae0b
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2020-11-14 22:48:50 +0300

    create balance process store struct

--- product-management/src/store/store.js
+++ product-management/src/store/store.js
@@ -3,11 +3,21 @@
 import Vuex from "vuex";
 import Vue from "vue";
 import product from "./modules/product";
+import * as getters from "./getters";
+import * as actions from "./actions";
+import * as mutations from "./mutations";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {},
+    state: {
+        purchase: 0.0,
+        sale: 0.0,
+        balance: 0.0,
+    },
+    getters,
+    mutations,
+    actions,
     modules: {
         product,
     }
