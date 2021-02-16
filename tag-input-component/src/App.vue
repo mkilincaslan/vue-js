50f628a93fa765843d333efaa261452ab2bf2d94
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-02-17 00:52:46 +0300

    add a new tag

--- tag-input-component/src/App.vue
+++ tag-input-component/src/App.vue
@@ -1,23 +1,39 @@
 <template>
     <div class="tag-container">
-        <span class="tag">
-            <span class="content">Vuejs</span>
+        <span class="tag" v-for="tag in tags" :key="tag">
+            <span class="content">{{ tag }}</span>
             <span class="close">X</span>
         </span>
-        <span class="tag">
-            <span class="content">Vuejs</span>
-            <span class="close">X</span>
-        </span>
-        <span class="tag">
-            <span class="content">Vuejs</span>
-            <span class="close">X</span>
-        </span>
+        <input type="text" @keydown.enter="createTag" />
+        <div class="error" v-show="error">
+            <span>{{ error }}</span>
+        </div>
     </div>
 </template>
 
 <script>
 
 export default {
+    data() {
+        return {
+            error: null,
+            tags: ['vuejs', 'nodejs', 'react']
+        }
+    },
+    methods: {
+        createTag(event) {
+            let tag = event.target;
+            if (tag.value !== '') {
+                if (this.tags.find(e => e.trim().toLowerCase() === tag.value.trim().toLowerCase())) {
+                    this.error = 'This tag is already added!';
+                } else {
+                    this.error = null;
+                    this.tags.push(tag.value);
+                    tag.value = '';
+                }
+            }
+        },
+    }
 }
 </script>
 
@@ -36,11 +52,25 @@ export default {
         color: #000;
         cursor: default;
         font-size: 14px;
+        margin: 0 5px;
+    }
+
+    input {
+        outline: none;
+        height: 30px;
+        width: 100px;
+    }
+
+    .error {
+        font-size: 12px;
+        color: red;
+        margin: 5px;
     }
 
     .tag .close {
         font-size: 12px;
         cursor: pointer;
+        padding: 0 3px;
     }
 
 </style>
