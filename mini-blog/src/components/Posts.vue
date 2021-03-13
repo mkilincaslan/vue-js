81c9a869ff4d970a2b8e0ad7baab7fd91ceee964
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2021-03-14 01:26:55 +0300

    mini blog begin

---
+++ mini-blog/src/components/Posts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container-fluid pt-5">
+    <h1 class="text-center">Yazı Listesi</h1>
+    <hr>
+    <div class="d-flex flex-wrap flex-row justify-content-center align-items-center">
+      <div class="card m-1" v-for="i in 5" :key="i">
+        <img class="card-img-top" src="https://blog.crazycoder.io/images/bg-header.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+  .card {
+    width: 300px;
+  }
+</style>
