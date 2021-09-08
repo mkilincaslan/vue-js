39f4f007fd77988e6e67d49f5d59e5d32cf769e4
Mesut <kilincaslanmesut@gmail.com>
2021-09-09 00:19:03 +0300

    Auth mini project with vue

---
+++ vue-js-auth/src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container-fluid">
+    <div class="row mt-5">
+      <div class="col-md-6 offset-3">
+        <h3>Hakkımda Sayfası</h3>
+        <p> Ben 22 yaşında <strong>Fırat Üniversitesi Yazılım Mühendisliği</strong> 4. sınıf öğrencisi bir yazılım geliştiricisiyim. Mobil uygulama geliştirme, web sitesi ve web servis hazırlama, sunucu yönetimi ve kurulum konularında tecrübeliyim. Linux kurulumu ve kullanımı konusunda 2.5 yıllık bir deneyime sahibim. Meraklı, hırslı ve özgüveni yüksek birisi olarak araştırmayı ve öğrenmeyi her koşulda ve şartta bırakmayan bir kişiliğe sahibim.</p>
+
+        <h4>Bugünlerde ise</h4>
+        <p>Kendi yazılım şirketimi kurmak üzere atılımlar yapmaya başladım</p>
+        <h4>Benim görüşüme göre</h4>
+        <blockquote>Herkes bildiğini paylaşırsa, herkes daha fazla öğrenecektir.</blockquote>
+      </div>
+    </div>
+  </div>
+</template>
