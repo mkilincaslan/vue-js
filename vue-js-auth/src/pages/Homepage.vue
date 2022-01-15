a2f04e53cf989942ded501936d594a6d8e9f3557
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-01-15 16:48:29 +0300

    vue-js-auth
    update homepage description

--- vue-js-auth/src/pages/Homepage.vue
+++ vue-js-auth/src/pages/Homepage.vue
@@ -3,7 +3,7 @@
     <div class="row mt-5">
       <div class="col-md-6 offset-3">
         <h3>Vue.js | Auth İşlemleri</h3>
-        <p>Bu bölge çok gizli bölgedir.. Ancak giriş yapanların görmesi gerekir. Ondan dolayı lütfen ilk olarak giriş yapınız..</p>
+        <p>Bu uygulamada hakkımda kısmı gizli bölgedir.. Ancak giriş yapanların görmesi gerekir. Ondan dolayı lütfen ilk olarak giriş yapınız..</p>
       </div>
     </div>
   </div>
