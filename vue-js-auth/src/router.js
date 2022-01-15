5b23325d78782f8f9abb8345f1a63905db02355f
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-01-15 16:09:37 +0300

    vue-js-auth
    add before enter control to the router

--- vue-js-auth/src/router.js
+++ vue-js-auth/src/router.js
@@ -5,13 +5,30 @@ import About from "./pages/About.vue";
 import Homepage from "./pages/Homepage.vue";
 import Auth from "./pages/auth/Auth.vue";
 
+import store from './store';
+
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
     routes: [
-        { path: "/about", component: About },
-        { path: "/auth", component: Auth },
-        { path: "/", component: Homepage },
+        { 
+            path: "/about", 
+            component: About, 
+            beforeEnter(to, from, next) {
+                store.getters.isAuthenticated ? next() : next('/auth');
+            } 
+        },
+        { 
+            path: "/", 
+            component: Homepage, 
+            beforeEnter(to, from, next) {
+                store.getters.isAuthenticated ? next() : next('/auth');
+            }  
+        },
+        { 
+            path: "/auth", 
+            component: Auth, 
+        },
     ],
     mode: "history"
 });
