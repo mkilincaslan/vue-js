6774fd862f7f95b5f2fda48f719b0a4c6b7aeda9
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-01-15 21:16:56 +0300

    vue-js-auth
    error messages management

--- vue-js-auth/src/pages/auth/Auth.vue
+++ vue-js-auth/src/pages/auth/Auth.vue
@@ -9,13 +9,15 @@
                 <hr>
                 <form @submit.prevent="onSubmit">
                     <div class="form-group">
-                        <label>E-posta Adresiniz</label>
-                        <input v-model="user.email" type="email" class="form-control"
-                               placeholder="E-posta adresinizi giriniz">
+                        <label :class="{ 'text-danger' : error.email }">E-posta Adresiniz</label>
+                        <input v-model="user.email" type="email" class="form-control" :class="{ 'is-invalid' : error.email }" placeholder="E-posta adresinizi giriniz">
                     </div>
                     <div class="form-group">
-                        <label>Şifre</label>
-                        <input v-model="user.password" type="password" class="form-control" placeholder="Şifreniz...">
+                        <label :class="{ 'text-danger' : error.password }">Şifre</label>
+                        <input v-model="user.password" type="password" class="form-control" :class="{ 'is-invalid' : error.password }" placeholder="Şifreniz...">
+                    </div>
+                    <div class="form-group" :v-show="errors">
+                        <p class='text-danger'>{{ error.message }}</p>
                     </div>
                     <div class="button-container d-flex  flex-column align-items-center">
                         <button type="submit" :class="{'btn-primary' : isUser, 'btn-success' : !isUser }"
@@ -39,17 +41,54 @@
                     email: null,
                     password: null
                 },
-                isUser: false
+                isUser: false,
+                error: {
+                    email: null,
+                    password: null,
+                    message: null,
+                }
             }
         },
         methods: {
             onSubmit() {
-                this.$store.dispatch("oAuth", { ...this.user, isUser: this.isUser })
-                    .then(() => {
-                        console.log(response);
-                        this.$router.push('/');
-                    });
+                this.$store.dispatch("oAuth", { ...this.user, isUser: this.isUser });
             }
+        },
+        computed: {
+            errors() {
+                const errors = this.$store.getters.getErrors;
+                const errorKey = Object.keys(errors).find(key => errors[key] !== null);
+                if (errorKey) {
+                    console.log(errors)
+                    this.error[errorKey] = true;
+                    this.error.message = errors[errorKey];
+                }
+                return errorKey ? true : false;
+            }
+        },
+        watch: {
+            isUser: function() {
+                this.error = {
+                    email: null,
+                    password: null,
+                    message: null,
+                };
+                this.user = {
+                    email: null,
+                    password: null
+                };
+                this.$store.dispatch('clearErrors');
+            },
+            'user.email': function () {
+                this.error['email'] = null;
+                this.error['message'] = null;
+                this.$store.dispatch('clearErrors');
+            },
+            'user.password': function () {
+                this.error['password'] = null;
+                this.error['message'] = null;
+                this.$store.dispatch('clearErrors');
+            },
         }
     }
 </script>
