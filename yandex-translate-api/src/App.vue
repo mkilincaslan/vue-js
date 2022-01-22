0adec5d9572704eee017492ac307682a7f4817b1
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-01-22 21:44:00 +0300

    create yandex translate app

---
+++ yandex-translate-api/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Vue-Translator</h1>
+    <h5 class="text-center">Vue.js ile Translator</h5>
+    <br>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <app-translate-form />
+            <h3 class="text-success text-center"></h3>
+            <app-history />
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    import TranslateFrom from './components/TranslateForm.vue';
+    import History from './components/History.vue';
+    export default {
+        name: 'app',
+        components: {
+            'app-translate-form': TranslateFrom,
+            'app-history': History,
+        }
+    }
+</script>
+
+<style>
+    body {
+        background-color: #dedede;
+    }
+</style>
---
+++ yandex-translate-api/src/components/TranslateForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <form @submit="translateIt" class="well">
+        <textarea cols="30" rows="5" class="form-control" placeholder="What would you like to translate..."></textarea>
+        <select class="form-control">
+            <option></option>
+        </select>
+        <br>
+        <div class="text-left">
+            <strong>Tespit Edilen Dil : </strong> 
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary btn-block">Çevir Gelsin!</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
