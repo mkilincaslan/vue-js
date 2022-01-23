da9ccb7ff7b560326075de4328d21b5bf69d018a
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-01-23 23:08:26 +0300

    yandex-translate-api
    define submit func & data variables

--- yandex-translate-api/src/components/TranslateForm.vue
+++ yandex-translate-api/src/components/TranslateForm.vue
@@ -1,12 +1,12 @@
 <template>
-    <form @submit="translateIt" class="well">
-        <textarea cols="30" rows="5" class="form-control" placeholder="What would you like to translate..."></textarea>
-        <select class="form-control">
-            <option></option>
+    <form @submit.prevent="translateIt" class="well">
+        <textarea v-model="translateText" cols="30" rows="5" class="form-control" placeholder="What would you like to translate..."></textarea>
+        <select v-model="translateTo" class="form-control">
+            <option>1</option>
         </select>
         <br>
         <div class="text-left">
-            <strong>Tespit Edilen Dil : </strong> 
+            <strong>Detected Lang : </strong> 
         </div>
         <br>
         <button type="submit" class="btn btn-primary btn-block">Çevir Gelsin!</button>
@@ -15,7 +15,16 @@
 
 <script>
     export default {
-        
+        data() {
+            return {
+                translateText: '',
+                translateTo: '',
+            }
+        },
+        methods: {
+            translateIt() {
+            }
+        }
     }
 </script>
 
