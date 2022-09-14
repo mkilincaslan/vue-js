255b8343c5090a917813ffe5272ace36cba9340a
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-09-15 00:28:05 +0300

    rename folder

---
+++ animations/V2/src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <transition name="toast">
+      <Toast v-if="showToast" />
+    </transition>
+    <Todos @badValue="triggerToast" />
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import Toast from '../components/Toast'
+import Todos from '../components/Todos'
+
+export default {
+  components: { Toast, Todos },
+  setup() {
+    const showToast = ref(false)
+
+    const triggerToast = () => {
+      showToast.value = true;
+      setTimeout(() => showToast.value = false, 3000)
+    }
+
+    return { showToast, triggerToast }
+  }
+}
+</script>
+
+<style>
+  .toast-enter-from {
+    opacity: 0;
+    transform: translateY(-60px);
+  }
+  /* .toast-enter-to {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .toast-enter-active, .toast-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  /* .toast-leave-from {
+    opacity: 1;
+    transform: translateY(0);
+  } */
+  .toast-leave-to {
+    opacity: 0;
+    transform: translateY(-60px);
+  }
+</style>
