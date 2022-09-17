3985805e94302c6730b5e4a036f3072b0a2f81c3
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-09-17 22:55:58 +0300

    using vue animation hooks with gsap animation package

--- animations/V2/src/views/About.vue
+++ animations/V2/src/views/About.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <transition
+      appear
+      @before-enter="beforeEnter"
+      @enter="enter"
+    >
+      <h1>About</h1>
+    </transition>
+
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -9,9 +16,25 @@
 
 <script>
 
+import gsap from 'gsap';
+
 export default {
   setup() {
+    const beforeEnter = (el) => {
+      el.style.transform = 'translateY(-60px)';
+      el.style.opacity = 0;
+    };
+    const enter = (el, done) => {
+      gsap.to(el, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+      });
+    };
 
+    return { beforeEnter, enter };
   }
 }
 </script>
