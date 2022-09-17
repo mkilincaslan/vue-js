411b3af22d24a53de0eb7a307cbd56fd23c62f6c
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2022-09-17 23:22:06 +0300

    animations between pages with transition and router

--- animations/V2/src/App.vue
+++ animations/V2/src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> | 
-    <router-link to="/contact">Contact</router-link>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/contact">Contact</router-link>
+    </div>
+    <router-view v-slot="{ Component }">
+      <transition name="route" mode="out-in">
+        <component :is="Component"></component>
+      </transition>
+    </router-view>
   </div>
-  <router-view/>
 </template>
 
 <style>
@@ -25,4 +31,17 @@ body {
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+  .route-enter-from, .route-leave-to {
+    opacity: 0;
+    transform: translateX(100px);
+  }
+  .route-enter-active, .route-leave-active {
+    transition: all 0.7s ease;
+  }
+
+  .route-leave-to {
+    opacity: 0;
+    transform: translateX(-100px);
+  }
 </style>
